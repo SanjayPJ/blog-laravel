e07ecec73c7f4d712cb0ae4eadd4c7c22e32b701
sanjaypj <sanjaypjayan2000@gmail.com>
2018-12-10 07:42:15 +0530

    initial commit

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+@section('content')
+<div class="jumbotron text-center">
+  <h1 class="display-4">{{ $title }}</h1>
+  <p class="lead">This is a simple laravel webapp with blog feature.</p>
+  <a class="btn btn-primary btn-lg" href="#" role="button">Login</a>
+  <a class="btn btn-success btn-lg" href="#" role="button">Register</a>
+</div>
+<h1></h1>
+
+@endsection
